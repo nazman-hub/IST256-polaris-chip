eebb4ec259589fd9944efaae7066df0d6c503c58
nazman-hub <123609252+nazman-hub@users.noreply.github.com>
2024-09-22 13:34:55 -0400

    completed assignment

--- src/my-card.js
+++ src/my-card.js
@@ -15,23 +15,87 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = "https://miro.medium.com/v2/resize:fit:1200/1*3fEz3HOci5JtFWRtixBwrw.png"
+    this.description = "Description";
+    this.link = "#";
+    this.linkText = "Link";
+    this.buttonLink = "#";
+    this.buttonText = "Button";
+    this.bgColor = "cyan";
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-flex;        
+
       }
+      
+      *{
+        margin: 0px;
+        padding: 0px;
+      }
+
+      .card{
+
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        padding: 32px;
+        background-color: cyan;
+        align-items: center;
+        gap: 16px;
+        margin:16px 16px;
+        font-family: "comic sans ms";
+        font-size: 16px;
+        text-align: center;     
+      }
+
+      .card img{
+        width: 300px;
+      }
+
+      a button{
+        padding: 12px 24px;
+        background: black;
+        border: none;
+        color: white;
+        font: inherit;
+      }
+
+
+      button:hover{
+        background: gray;
+        cursor: pointer;
+      }
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+        <div class="card">
+          <img src="${this.image}" class ="image item" width="300px">
+          <h1 class="title item">${this.title}</h1>
+          <p class="description item">${this.description}</p>
+          <a href="${this.link}" class="link item" target="_blank">${this.linkText}</a>
+          <a href="${this.buttonLink}" class="item" target="_blank">
+            <button class="details-button item">${this.buttonText}</button>
+          </a>
+  </div>
+      `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      description: { type: String },
+      link: { type: String },
+      linkText: { type: String },
+      buttonText: { type: String },
+      buttonLink: { type: String },
+      bgColor: { type: String }
     };
   }
 }
--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -90,7 +90,7 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<a href="${this.link}">${this.title}</span>`;
+    return html`<span><a href="${this.link}">${this.title}</span>`;
   }
 
   // LitElement uses the properties call to do the following:
